bdd591753dab41b52f6528392edcb1201dab8eaf
suno-o <124753445+suno-o@users.noreply.github.com>
2023-04-21 00:43:12 -0400

    dummy-feat(login&logout): Mimic login and logout feature

--- src/components/Layout/components/NavBar/index.tsx
+++ src/components/Layout/components/NavBar/index.tsx
@@ -1,17 +1,51 @@
+import { useState, useEffect } from "react"
 import Link from "next/link"
-import Button from "@/components/Button";
+import Button from "@/components/Button"
 import {
   NavBarContainer,
   Links,
 } from "./styles"
 
-const NavBar = () => (
-  <NavBarContainer>
-    <h2>Logo</h2>
-    <p style={{textAlign: 'center', fontWeight: 'bold'}}>BooKee is currently under development</p>
-    <Links>
-    <Link href='/login'><Button bgTheme='primary'>Log in</Button></Link></Links>
-  </NavBarContainer>
-)
+/* ===== DUMMY: remove later ===== */
+const login = () => {
+  localStorage.setItem('dummy-bookee-user', 'something');
+}
+const logout = () => {
+  localStorage.removeItem('dummy-bookee-user');
+}
+/* ================================= */
+
+const NavBar = () => {
+  /* ===== REMOVE later: just temporarily mimicking login and logout ===== */
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    const u = localStorage.getItem('dummy-bookee-user') || '';
+    if (u) setUser(u);
+  }, [])
+  /* ===================================================================== */
+
+  return (
+    <NavBarContainer>
+      <h2>Logo</h2>
+      {user && (
+        <Links>
+          Style these later:
+          <Link href='/dashboard'>Dashboard</Link>
+          <Link href='/transactions'>Transactions</Link>
+          <Link href='/credit-bill-calculator'>Credit bill calculator</Link>
+        </Links>
+      )}
+        <p style={{textAlign: 'center', fontWeight: 'bold'}}>BooKee is currently under development</p>
+      <Links>
+      {!user ? (
+        <Link href='/login' onClick={login}><Button bgTheme='primary'>Log in</Button></Link>
+      ) : (
+        <Link href='/' onClick={logout}><Button bgTheme='primary'>Log out</Button></Link>
+      )}
+      </Links>
+    </NavBarContainer>
+  )
+}
 
 export default NavBar;
